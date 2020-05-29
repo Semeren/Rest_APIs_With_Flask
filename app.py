8f94da04364fab7c253d685b0d5ae29dce841549
Justin Hack <jhack@theamegroup.com>
2020-05-28 22:02:21 -0500

    Resolving db.init problem for Heroku

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+
 
 from security import authenticate,identity
 from resources.user import UserRegister
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'someformofsecretkey'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app,authenticate,identity)
 
